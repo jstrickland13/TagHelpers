d09dbb4178d3715601d3b2136b7542dfef39b3a8
jstrickland13@gmail.com <jstrickland13@gmail.com>
2020-06-23 16:54:31 -0400

    Add a tag helper to turn SVGs into Icons

---
+++ SvgTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace WebApplication1.TagHelpers
+{
+    public class IconTagHelper : TagHelper
+    {
+        public string Src { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!String.IsNullOrEmpty(Src))
+            {
+                output.TagName = "span";
+                output.AddClass("icon", HtmlEncoder.Default);
+                string file = File.ReadAllText($"wwwroot/lib/bootstrap-icons-1.0.0-alpha4/{this.Src}.svg");
+                output.Content.SetHtmlContent(file);
+            }
+        }
+    }
+
+    public class IconLinkTagHelper : TagHelper
+    {
+        public string Src { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!String.IsNullOrEmpty(Src))
+            {
+                output.TagName = "a";
+                output.AddClass("icon-link", HtmlEncoder.Default);
+                string file = File.ReadAllText($"wwwroot/lib/bootstrap-icons-1.0.0-alpha4/{this.Src}.svg");
+                output.Content.SetHtmlContent(file);
+            }
+        }
+    }
+}
